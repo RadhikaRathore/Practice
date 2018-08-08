118c1542ec93be543aa697d88e2cae27677773ae
RadhikaRathore <radhikarathore7@gmail.com>
2018-08-09 00:37:01 +0530

    frist commit

---
+++ Practice/move all 0s to front(of list).py
@@ -0,0 +1,13 @@
+arr = [1, 0, 2, 0, 1, 0]
+k = 0
+temp = 0
+arrlen = len(arr)
+
+for i in range(arrlen):
+    if arr[i] == 0:
+        temp = arr[k]
+        arr[k] = arr[i]
+        arr[i] = temp
+        k += 1
+
+print(arr)
